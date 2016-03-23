9e2c3b3d82c90ded95ed7de68747196ceeb99de0
brianaoree <oreebriana@yahoo.com>
2016-03-23 14:29:37 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+    Author: Abbas Abdulmalik
+    Creation Date: 
+    Title: 
+    Revised: 
+    Purpose: 
+    Notes:
+*/
+//=========CONSTANTS AND TABLES=========
+
+
+//=========STATE VARIABLES==============
+
+
+//=========THE DRIVER'S SEAT============
+
+
+
+//=========UNDER THE HOOD===============
